b1e4771585e091a1d6ae68d655dfdcd34a526254
alan-cy-lai <alan@ergostratus.com>
2015-01-14 21:18:39 -0800

    Complete base Lab02
    Signed-off-by: alan-cy-lai <alan@ergostratus.com>

---
+++ application/views/gallery.php
@@ -0,0 +1,5 @@
+
+<table cols="3" class="gallery">
+    {thetable}
+</table>
+<h1>The image gallery, complete :)</h1>
---
+++ application/views/welcome.php
@@ -0,0 +1,5 @@
+<table cols="3" class="gallery">
+    {thetable}
+    <h1>This is just a simple image gallery :)</h1>
+    <p>The images above are the three most recently posted.</p>
+</table>
